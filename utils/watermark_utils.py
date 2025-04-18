64c9ede73d4e36e7a02e8c79d3a6e7fa77ee9a82
Natalia2-pixel <x23351560@student.ncirl.ie>
2025-04-18 11:48:52 +0100

    Update watermark_utils.py
    Major changes - used two different functions and a wrapper function

--- utils/watermark_utils.py
+++ utils/watermark_utils.py
@@ -16,79 +16,93 @@ xception_model.compile(optimizer='adam', loss='mse')
 cgan_generator = tf.keras.models.load_model("Models/cgan_generator_model.h5", compile=False)
 cgan_generator.compile(optimizer='adam', loss='mse')
 
-def apply_watermark_with_model(image_path=None, model=None, cover_image=None, custom_watermark_img=None):
-    # Resize and convert to grayscale 64x64
-    cover_image = cover_image.convert("L").resize((64, 64))
-    custom_watermark_img = custom_watermark_img.convert("L").resize((64, 64)) if custom_watermark_img else Image.new("L", (64, 64), color=0)
+def apply_watermark_ml_model(cover_image, watermark_image=None, model_type="SVM", alpha=0.3):
+    if cover_image is None:
+        raise ValueError("Cover image is missing.")
+    if watermark_image is None:
+        watermark_image = Image.new("L", (64, 64), color=0)
+    cover = cover_image.convert("L").resize((64, 64))
+    mark = watermark_image.convert("L").resize((64, 64))
 
-    # Convert to numpy arrays
-    cover_np = np.array(cover_image).astype(np.float32) / 255.0
-    watermark_np = np.array(custom_watermark_img).astype(np.float32) / 255.0
+    cover_np = np.array(cover, dtype=np.float32) / 255.0
+    mark_np = np.array(mark, dtype=np.float32) / 255.0
 
-    # Force correct shapes
-    if cover_np.shape != (64, 64):
-        raise ValueError(f"Cover image shape incorrect: {cover_np.shape}, expected (64, 64)")
-    if watermark_np.shape != (64, 64):
-        raise ValueError(f"Watermark image shape incorrect: {watermark_np.shape}, expected (64, 64)")
+    blended_gt = (1 - alpha) * cover_np + alpha * mark_np
+    input_vec = np.hstack([cover_np.flatten(), mark_np.flatten()])
+    if watermark_image is None:
+        watermark_image = Image.new("L", (64, 64), color=0)
+    if input_vec.shape[0] != pca_x.components_.shape[1]:
+        raise ValueError(f"Input vector length mismatch for PCA: {input_vec.shape[0]}")
 
-    # Blend as ground truth
-    blended_gt = (0.7 * cover_np + 0.3 * watermark_np)
+    reduced_input = pca_x.transform([input_vec])
 
-    # Flatten
-    cover_flat = cover_np.flatten()
-    mark_flat = watermark_np.flatten()
+    if model_type == "SVM":
+        predicted = svm_model.predict(reduced_input)
+    elif model_type == "GBM":
+        predicted = gbm_model.predict(reduced_input)
+    else:
+        raise ValueError("Unknown ML model type")
 
-    if model == "SVM" or model == "GBM":
-        combined_input = np.hstack([cover_flat, mark_flat])  # Expecting length = 4096 + 4096 = 8192
-        expected_input_size = pca_x.components_.shape[1]
-        if combined_input.shape[0] != expected_input_size:
-            raise ValueError(f"Input size {combined_input.shape[0]} does not match PCA expected input size {expected_input_size}")
-    
-        reduced_input = pca_x.transform([combined_input])
-    
-        if model == "SVM":
-            predicted_compressed = svm_model.predict(reduced_input)
-        else:
-            predicted_compressed = gbm_model.predict(reduced_input)
+    reconstructed = pca_y.inverse_transform(predicted)[0]
+    predicted_image = reconstructed.reshape(64, 64)
 
-        predicted_flat = pca_y.inverse_transform(predicted_compressed)[0]
+    mse = np.mean((blended_gt - predicted_image) ** 2)
+    psnr = 10 * np.log10(1.0 / mse)
+    ssim_score = ssim(blended_gt, predicted_image, data_range=1.0)
 
-    elif model == "GBM":
-        combined_input = np.hstack([cover_flat, mark_flat])
-        if combined_input.shape[0] != pca_x.n_features_:
-            raise ValueError(f"GBM input length {combined_input.shape[0]} doesn't match PCA expected {pca_x.n_features_}")
-        reduced_input = pca_x.transform([combined_input])
-        predicted_compressed = gbm_model.predict(reduced_input)
-        predicted_flat = pca_y.inverse_transform(predicted_compressed)[0]
+    return Image.fromarray((predicted_image * 255).astype(np.uint8)), {"mse": mse, "psnr": psnr, "ssim": ssim_score}
 
-    elif model == "CNN":
-        stacked_input = np.stack([cover_np, watermark_np], axis=-1)
-        input_tensor = np.expand_dims(stacked_input, axis=0)
-        predicted = cnn_model.predict(input_tensor, verbose=0)
-        predicted_flat = predicted[0].squeeze()
+def apply_watermark_dl_model(cover_image, watermark_image=None, model_type="CNN", alpha=0.3):
+    if cover_image is None:
+        raise ValueError("Cover image is missing.")
+    if watermark_image is None:
+        watermark_image = Image.new("L", (64, 64), color=0)
+    cover = cover_image.convert("L").resize((64, 64))
+    mark = watermark_image.convert("L").resize((64, 64)) if watermark_image else Image.new("L", (64, 64))
 
-    elif model == "Xception":
-        input_tensor = tf.image.grayscale_to_rgb(tf.convert_to_tensor(cover_np[..., np.newaxis]))
-        input_tensor = tf.image.resize(input_tensor, [71, 71])
-        input_tensor = tf.expand_dims(input_tensor, axis=0)
-        predicted = xception_model.predict(input_tensor, verbose=0)
-        predicted_flat = tf.image.resize(predicted[0], [64, 64]).numpy().squeeze()
+    cover_np = np.array(cover, dtype=np.float32) / 255.0
+    mark_np = np.array(mark, dtype=np.float32) / 255.0
+    blended_gt = (1 - alpha) * cover_np + alpha * mark_np
+    if watermark_image is None:
+        watermark_image = Image.new("L", (64, 64), color=0)
+    if model_type == "CNN":
+        stacked = np.stack([cover_np, mark_np], axis=-1)
+        input_tensor = np.expand_dims(stacked, axis=0)
+        predicted = cnn_model.predict(input_tensor, verbose=0)[0].squeeze()
 
-    elif model == "GAN":
-        stacked_input = np.stack([cover_np, watermark_np], axis=-1)
-        input_tensor = tf.convert_to_tensor(stacked_input[np.newaxis, ...])
-        predicted = cgan_generator.predict(input_tensor, verbose=0)
-        predicted_flat = predicted[0].squeeze()
+    elif model_type == "Xception":
+        rgb_tensor = tf.image.grayscale_to_rgb(tf.convert_to_tensor(cover_np[..., np.newaxis]))
+        resized = tf.image.resize(rgb_tensor, [71, 71])
+        input_tensor = tf.expand_dims(resized, axis=0)
+        predicted = xception_model.predict(input_tensor, verbose=0)[0]
+        predicted = tf.image.resize(predicted, [64, 64]).numpy().squeeze()
+
+    elif model_type == "GAN":
+        stacked = np.stack([cover_np, mark_np], axis=-1)
+        input_tensor = tf.convert_to_tensor(stacked[np.newaxis, ...])
+        predicted = cgan_generator.predict(input_tensor, verbose=0)[0].squeeze()
 
     else:
-        raise ValueError("Unsupported model selected")
+        raise ValueError("Unsupported DL model selected")
 
-    # Compute metrics
-    mse = np.mean((blended_gt.flatten() - predicted_flat.flatten()) ** 2)
+
+    mse = np.mean((blended_gt - predicted) ** 2)
     psnr = 10 * np.log10(1.0 / mse)
-    ssim_score = ssim(blended_gt, predicted_flat, data_range=1.0)
+    ssim_score = ssim(blended_gt, predicted, data_range=1.0)
 
-    # Convert predicted image back to PIL
-    pred_img = Image.fromarray((predicted_flat * 255).astype(np.uint8))
+    return Image.fromarray((predicted * 255).astype(np.uint8)), {"mse": mse, "psnr": psnr, "ssim": ssim_score}
 
-    return pred_img, {"mse": mse, "psnr": psnr, "ssim": ssim_score}
+def apply_watermark_with_model(model, cover_image, custom_watermark_img=None):
+    if cover_image is None:
+        raise ValueError("Cover image is missing. Please upload one.")
+    if model in ["SVM", "GBM"]:
+        return apply_watermark_ml_model(cover_image, custom_watermark_img, model_type=model)
+    elif model in ["CNN", "Xception", "GAN"]:
+        return apply_watermark_dl_model(cover_image, custom_watermark_img, model_type=model)
+    else:
+        raise ValueError("Unknown model selected")
+
+
+
+    
+    
